81e2ce5e3726260d438a568121b1c5b002ce6354
lugetego <lugetego123./>
2021-11-19 12:01:19 -0600

    Commit inicial

---
+++ assets/controllers/modal-form_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from 'stimulus';
+import { Modal } from 'bootstrap';
+import $ from 'jquery';
+
+export default class extends Controller {
+    static targets = ['modal', 'modalBody'];
+    static values = {
+        formUrl: String,
+    }
+
+    async openModal(event) {
+        this.modalBodyTarget.innerHTML = 'Loading...';
+        const modal = new Modal(this.modalTarget);
+        modal.show();
+
+        this.modalBodyTarget.innerHTML = await $.ajax(this.formUrlValue);
+    }
+}
